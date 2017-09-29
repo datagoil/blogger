fc4e2b3f1b34208abee780dee3d8f52aa6a7b343
Sara S <ida.azalia@protonmail.com>
2017-09-29 10:57:09 -0500

    Sorcery auth completed

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,9 @@ resources :articles do
     resources :comments
 end
 resources :tags
+resources :authors
+resources :author_sessions, only: [ :new, :create, :destroy ]
+
+get 'login'  => 'author_sessions#new'
+get 'logout' => 'author_sessions#destroy'
 end
